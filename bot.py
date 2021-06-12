a19d491a1ca061eec484428e7db626b9badca677
vsheel <vsheel@cisco.com>
2021-06-12 06:39:50 +0530

    Adding help command

--- bot.py
+++ bot.py
@@ -183,7 +183,7 @@ def get_available_slots(chat_id, dist_id, dose_type, age, check_date):
         resp = get_availability_from_data(data, dose_type, age)
     # print(resp)
     if resp is not None and len(resp) > 0:
-        print('Slots found for ', chat_id)
+        print('Slots found for ', chat_id, ' for total ', len(resp), ' days')
         bot.send_message(chat_id, text=format_message(resp, dose_type), parse_mode="HTML")
     else:
         print('No slots found for ', chat_id, "on next 7 days of ", check_date)
