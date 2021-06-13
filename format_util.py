8ebdb786fcc12cb3ad99661b2bcf4fd086cf336d
vsheel <vsheel@cisco.com>
2021-06-13 17:14:13 +0530

    Saving as per new user fields

--- format_util.py
+++ format_util.py
@@ -21,7 +21,7 @@ def format_message(resp, dose_type):
 def format_user_details(user):
     message = "<b> Your saved choice: </b>\n\n"
     message += "- Age : " + str(user['age']) + '\n'
-    message += "- Vaccine: " + user['vaccine'] + '\n'
+    message += "- Vaccine: " + str(user['vaccine']) + '\n'
     message += "- Fee: " + str(user['fee_type']) + '\n'
     message += "- Dose : " + str(user['dose_type']) + '\n'
     message += "- Notify for slots : " + str(user['notify']) + '\n'
